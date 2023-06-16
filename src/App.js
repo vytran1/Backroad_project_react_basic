3e51a001fa95a8e6ca0c68083b1dcbd733219992
vytran1 <vy.tn171003@gmail.com>
2023-06-16 12:18:00 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/hero";
+import Navbar from "./components/navbar";
+import Services from "./components/services";
+import Tours from "./components/Tours";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import { socialLinks } from "../data";
+import PageLinks from "./PageLinks.js";
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+
+      <ul className="footer-icons">
+        {socialLinks.map((social) => {
+          const { id, href, icon } = social;
+          return (
+            <li key={id}>
+              <a href={href} target="_blank" className="footer-icon">
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date"></span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
